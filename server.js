4cd4e44bf16715c83e652065d46fbd93e411e05a
Matthew Stinis <mpstinis@gmail.com>
2019-11-25 18:36:25 -0500

    added libraries to server file

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
-let express = require('express')
-let app = express()
-let reloadMagic = require('./reload-magic.js')
+let express = require("express");
+let app = express();
+let reloadMagic = require("./reload-magic.js");
+let MongoClient = require("mongodb".MongoClient);
+let ObjectID = require("mongodb").ObjectID;
+let multer = require("multer");
+let upload = multer({ dest: __dirname + "/uploads/" });
+app.use("/uploads", express.static("uploads"));
+let dbo = undefined;
+let url =
+  "mongodb+srv://mstinis:eR5w+i^<*A@cluster0-xb9sp.mongodb.net/test?retryWrites=true&w=majority";
+MongoClient.connect(url, { useNewUrlParser: true }, (err, db) => {
+  dbo = db.db("event-horizon");
+});
 
-reloadMagic(app)
+reloadMagic(app);
 
-app.use('/', express.static('build')); // Needed for the HTML and JS files
-app.use('/', express.static('public')); // Needed for local assets
+app.use("/", express.static("build")); // Needed for the HTML and JS files
+app.use("/", express.static("public")); // Needed for local assets
 
 // Your endpoints go after this line
 
 // Your endpoints go before this line
 
-app.all('/*', (req, res, next) => { // needed for react router
-    res.sendFile(__dirname + '/build/index.html');
-})
+app.all("/*", (req, res, next) => {
+  // needed for react router
+  res.sendFile(__dirname + "/build/index.html");
+});
 
-
-app.listen(4000, '0.0.0.0', () => { console.log("Server running on port 4000") })
+app.listen(4000, "0.0.0.0", () => {
+  console.log("Server running on port 4000");
+});
